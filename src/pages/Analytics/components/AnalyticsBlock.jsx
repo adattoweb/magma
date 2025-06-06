045eda752d6d18da4da33738ec79b2e2df0106e8
adattoweb <adattostudio174@gmail.com>
2025-06-06 14:25:27 +0300

    v0.6.10 imroved analytics

--- src/pages/Analytics/components/AnalyticsBlock.jsx
+++ src/pages/Analytics/components/AnalyticsBlock.jsx
@@ -1,32 +1,41 @@
 import conSecTime from "../helpers/conSecTime";
 import formatDate from "../helpers/formatDate"
 
-export default function AnalyticsBlock({ children, date, allTime, maxHeight, isGray}) {
-    console.log("AnalyticsBlock render")
-    if(isGray){
+import AnalyticsItem from "./AnalyticsItem"
+
+export default function AnalyticsBlock({ date, allTime, max, maxHeight, isGray, objectTasks, project, uniqueArr }) {
+    function BlockLayout({ children }) {
         return (
             <div className="aitem">
-                <h4>{conSecTime(allTime)}</h4>
                 <div className="aitem__block" style={{ height: maxHeight + "px" }}>
-                    <div className='aitem__color' style={{ backgroundColor: "#333", height: maxHeight + "px" }}></div>
+                    {children}
                 </div>
                 <div className="aitem__info">
-                    <h3>{date}</h3>
+                    <h3>{conSecTime(allTime)}</h3>
                     <h3>{formatDate(date)}</h3>
                 </div>
             </div>
-        );  
+        )
     }
+    console.log("AnalyticsBlock render")
+    if (isGray) {
+        return (
+            <BlockLayout><div className='aitem__color' style={{ backgroundColor: "#333", height: maxHeight + "px" }}></div></BlockLayout>
+        );
+    }
+    const blockHeight = maxHeight / (max / allTime)
+    console.log(blockHeight, maxHeight, max, allTime)
     return (
-        <div className="aitem">
-            <h4>{conSecTime(allTime)}</h4>
-            <div className="aitem__block" style={{ height: maxHeight + "px" }}>
-                {children}
-            </div>
-            <div className="aitem__info">
-                <h3>{date}</h3>
-                <h3>{formatDate(date)}</h3>
-            </div>
-        </div>
-    );             
+        <BlockLayout>{Object.keys(objectTasks).map(NU =>
+            objectTasks[NU].map(key => {
+                let arrLocal = localStorage.getItem(key).split("^");
+                let time = +arrLocal[4];
+                if (time === 0) return;
+                let elProject = arrLocal[1];
+                if (elProject !== project && project !== "Всі") return;
+                return <AnalyticsItem key={key} local={localStorage.getItem(key)} allTime={allTime} maxHeight={blockHeight} uniqueArr={uniqueArr} />;
+            })
+        )}
+        </BlockLayout>
+    );
 }
