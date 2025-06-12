0f516a921c44ce5bdf12a2804db9139c5344f24c
adattoweb <adattostudio174@gmail.com>
2025-06-12 22:07:05 +0300

    optimize calendar (function setPos)

--- src/pages/Calendar/Calendar.jsx
+++ src/pages/Calendar/Calendar.jsx
@@ -1,6 +1,7 @@
-import getDayDiff from "../../helpers/getDayDiff";
 import CalendarDay from "./ui/CalendarDay";
-import { useState, useEffect } from "react";
+import getDayDiff from "../../helpers/getDayDiff";
+import throttle from "../../helpers/throttle";
+import { useState, useEffect, useCallback } from "react";
 import "./Calendar.css";
 
 export default function Calendar(){
@@ -69,10 +70,10 @@ export default function Calendar(){
         return dateA - dateB;
     });
 
+    const handleMouseMove = useCallback(throttle((e) => {
+        setPos({ x: e.clientX, y: e.clientY });
+      }, 8), []); // 125 FPS
     useEffect(() => {
-        const handleMouseMove = (event) => {
-            setPos({ x: event.clientX, y: event.clientY });
-        };
         window.addEventListener("mousemove", handleMouseMove);
         return () => {
             window.removeEventListener("mousemove", handleMouseMove);
