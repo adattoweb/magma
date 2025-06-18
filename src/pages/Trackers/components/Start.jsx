1ec1c291ce220319fee3a229b165cebe87316dfa
adattoweb <adattostudio174@gmail.com>
2025-06-18 12:24:20 +0300

    v0.7.7 fixed bugs

--- src/pages/Trackers/components/Start.jsx
+++ src/pages/Trackers/components/Start.jsx
@@ -1,20 +1,18 @@
-import pause from "../../../assets/pause.png";
-import stoppause from "../../../assets/pause2.png";
+import stoppause from "../../../assets/pause.png";
+import pause from "../../../assets/pause2.png";
 import start from "../../../assets/start.png";
 
-import { useState, useEffect } from "react";
 import formatTime from "../../../helpers/formatTime";
+import { useState, useEffect } from "react";
 
-export default function Start({ changeTime, addItem, devAdd }) {
+export default function Start({ time, setTime, addItem, devAdd }) {
     const [isTimeRunning, setIsTimeRunning] = useState(false);
-    const [time, setTime] = useState(+localStorage.getItem("tracker-time"));
 
     useEffect(() => {
         let timer;
         if (isTimeRunning) {
             timer = setInterval(() => {
                 setTime((prev) => prev + 1);
-                changeTime((time + 1));
                 localStorage.setItem("tracker-time", time+1)
             }, 1000);
         } else {
@@ -23,22 +21,27 @@ export default function Start({ changeTime, addItem, devAdd }) {
         return () => clearInterval(timer);
     }, [isTimeRunning, time]);
 
+    function changeTimeRunning() {
+        if(time === 0){ // якщо ми починаємо відслідковувати час, то запам'ятаємо початок, щоб потім коректно відобразити
+            const now = new Date();
+            let startTime = `${String(now.getHours()).padStart(2, "0")}:${String(now.getMinutes()).padStart(2, "0")}`;
+            localStorage.setItem("tracker-start", startTime)
+        }
+        setIsTimeRunning(!isTimeRunning)
+    }
+
+    function stopTracker(){
+        addItem();
+        setTime(0);
+        setIsTimeRunning(false);
+    }
+
     return (
         <div className="trackers__start">
             <p>{formatTime(time)}</p>
-            <img draggable={false} onClick={() => {
-                setIsTimeRunning(!isTimeRunning)
-                if(!isTimeRunning){
-                    const now = new Date();
-                    localStorage.setItem("start-time", `${String(now.getHours()).padStart(2, "0")}:${String(now.getMinutes()).padStart(2, "0")}`);
-                }
-            }} src={isTimeRunning ? stoppause : start} alt="почати" onDragOver={devAdd} />
+            <img draggable={false} onClick={changeTimeRunning} src={isTimeRunning ? pause : start} alt="start tracking" onDragOver={devAdd} />
             {time > 0 &&
-                <img draggable={false} onClick={() => {
-                    addItem();
-                    setTime(0);
-                    setIsTimeRunning(false);
-                }} src={pause} alt="почати" />}
+                <img draggable={false} onClick={stopTracker} src={stoppause} alt="stop tracking" />}
         </div>
     );
 }
--- src/pages/Trackers/components/TrackerStart.jsx
+++ src/pages/Trackers/components/TrackerStart.jsx
@@ -6,13 +6,11 @@ export default function TrackerStart({changeAdd}) {
 
     const isEn = localStorage.getItem("settings-lang") === "en";
 
-
     const [name, setName] = useState(localStorage.getItem("tracker-name"));
     const [project, setProject] = useState(localStorage.getItem("tracker-project") === null ? "Без проєкту" : localStorage.getItem("tracker-projects").split("^")[0]); // Треба зробити щоб спочатку було СТАНДАРТНЕ ЗНАЧЕННЯ коли будуть проекти зберігатися, array[0]
     const changeProject = value => setProject(value);
 
     const [time, setTime] = useState(+localStorage.getItem("tracker-time"));
-    const changeTime = value => setTime(value);
 
     let startTime = "00:00";
     let endTime = "00:00";
@@ -20,14 +18,11 @@ export default function TrackerStart({changeAdd}) {
         let smallName = name
         if(name.length > 30) smallName = name.substring(0, 29) + "..."
         let newName = smallName?? smallName.replace(/(@|\^)+/g, ".");
-        startTime = localStorage.getItem("start-time");
+        startTime = localStorage.getItem("tracker-start") ?? "00:00"
         const now = new Date();
         endTime = `${String(now.getHours()).padStart(2, "0")}:${String(now.getMinutes()).padStart(2, "0")}`;
         let mustName = name.length === 0 ? isEn ? "Without name" : "Без назви" : newName;
-        let index = localStorage.getItem("tracker-index");
-        if(index === null){
-            index = "0";
-        }
+        let index = localStorage.getItem("tracker-index") ?? "0";
         localStorage.setItem(`tracker-item${index}^${now.getDate()}.${now.getMonth()+1}.${now.getFullYear()}`, `${mustName}^${project}^${startTime}^${endTime}^${time}`);
         localStorage.setItem("tracker-index", +index + 1 + "");
         setTime(0);
@@ -52,7 +47,7 @@ export default function TrackerStart({changeAdd}) {
                 }} value={name} />
                 <Dropdown changeProject={changeProject}/>
             </div>
-            <Start changeTime={changeTime} addItem={addItem} devAdd={() => devAdd(0)}/>
+            <Start time={time} setTime={setTime} addItem={addItem} devAdd={() => devAdd(0)}/>
         </div>
     );
 }
