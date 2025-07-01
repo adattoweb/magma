25cb44770e9ca04b6da81551dc2cccf590bb4627
adattoweb <adattostudio174@gmail.com>
2025-07-01 13:33:46 +0300

    testing touchmove

--- src/pages/Calendar/hooks/useMove.js
+++ src/pages/Calendar/hooks/useMove.js
@@ -5,27 +5,37 @@ export default function useMove(setPos, draggingCount){
 
     const isMobile = window.innerWidth <= 768;
 
-    let eventForDrag = 'mousemove';
-    if(isMobile) eventForDrag = 'touchmove'
-
     const rendersCount = useRef(0)
     
     const onMouseMove = useRef()
+    const onTouchMove = useRef()
 
     useEffect(() => {
         onMouseMove.current = throttle((e) => {
           setPos({ x: e.clientX, y: e.clientY });
           console.log(`pos renders: ${++rendersCount.current}`);
         }, 10);
+        onTouchMove.current = throttle((e) => {
+            const touch = e.touches[0]
+            setPos({x: touch.clientX, y: touch.clientY})
+        }, 10)
     }, []);
 
     useEffect(() => {
         if(draggingCount > 0){
-            window.addEventListener(eventForDrag, onMouseMove.current);
+            if(!isMobile){
+                window.addEventListener("mousemove", onMouseMove.current);
     
-            return () => {
-                window.removeEventListener(eventForDrag, onMouseMove.current);
-            };
+                return () => {
+                    window.removeEventListener("mousemove", onMouseMove.current);
+                };
+            } else {
+                window.addEventListener("touchmove", onTouchMove.current);
+    
+                return () => {
+                    window.removeEventListener("touchmove", onTouchMove.current);
+                };
+            }
         }
     }, [draggingCount])
 }
