551c22eaf84fcb4d86ed32653506f1758d687461
adattoweb <adattostudio174@gmail.com>
2025-09-12 18:27:30 +0300

    added a image in custom theme while editing or creating new custom theme for better experience

--- src/pages/Settings/components/Custom/CustomItem.jsx
+++ src/pages/Settings/components/Custom/CustomItem.jsx
@@ -80,15 +80,18 @@ export default function CustomItem({name, src, setArray, onChange}) {
             <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
                 <div className="tmodal">
                     <h3>{isEn ? "Edit theme" : "Редагування теми"}</h3>
-                    <div className="tmodal__inputs">
-                        <div className="tmodal__input">
-                            <label htmlFor="tname">{isEn ? "Name" : "Назва"}</label>
-                            <input placeholder="Theme 0" type="text" maxLength="15" className="tmodal__name" id="name" style={{ border: error && errorRef.current === 1 ? "1px solid #c52d2d" : isDark ? "1px solid #353535" : "1px solid #bababa" }} value={modalName} onChange={(e) => setModalName(e.target.value)} />
+                    <div className="cmodal__edit">
+                        <div className="tmodal__inputs">
+                            <div className="tmodal__input">
+                                <label htmlFor="tname">{isEn ? "Name" : "Назва"}</label>
+                                <input placeholder="Theme 0" type="text" maxLength="15" className="tmodal__name" id="name" style={{ border: error && errorRef.current === 1 ? "1px solid #c52d2d" : isDark ? "1px solid #353535" : "1px solid #bababa" }} value={modalName} onChange={(e) => setModalName(e.target.value)} />
+                            </div>
+                            <div className="tmodal__input">
+                                <label htmlFor="tname">{isEn ? "Image URL" : "Посилання на зображення"}</label>
+                                <input placeholder="https://images.unsplash.com/photo-1627797427417-ef69f1c4705b?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" type="text" className="tmodal__name" id="url" style={{ border: error && errorRef.current === 2 ? "1px solid #c52d2d" : isDark ? "1px solid #353535" : "1px solid #bababa" }} value={modalSrc} onChange={(e) => setModalSrc(e.target.value)} />
+                            </div>
                         </div>
-                        <div className="tmodal__input">
-                            <label htmlFor="tname">{isEn ? "Image URL" : "Посилання на зображення"}</label>
-                            <input placeholder="https://images.unsplash.com/photo-1627797427417-ef69f1c4705b?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" type="text" className="tmodal__name" id="url" style={{ border: error && errorRef.current === 2 ? "1px solid #c52d2d" : isDark ? "1px solid #353535" : "1px solid #bababa" }} value={modalSrc} onChange={(e) => setModalSrc(e.target.value)} />
-                        </div>
+                        {modalSrc.length > 0 && <img className="cmodal__img" src={modalSrc}/>}
                     </div>
                     <div className="gmodal__buttons">
                         <motion.div whileHover={{ scale: 1.05 }} className="tmodal__delete" onClick={deleteFunc}>{isEn ? "Delete" : "Видалити"}</motion.div>
--- src/pages/Settings/components/Custom/CustomModal.jsx
+++ src/pages/Settings/components/Custom/CustomModal.jsx
@@ -8,15 +8,18 @@ export default function CustomModal({ isOpen, setIsOpen, isEn, error, name, setN
         <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
         <div className="tmodal">
             <h3>{isEn ? "Create a theme" : "Створення теми"}</h3>
-            <div className="tmodal__inputs">
-                <div className="tmodal__input">
-                    <label htmlFor="tname">{isEn ? "Name" : "Назва"}</label>
-                    <input placeholder="Theme 0" type="text" maxLength="15" className="tmodal__name" id="name" style={{border: error && errorRef.current === 1 ?  "1px solid #c52d2d" : isDark ? "1px solid #353535" : "1px solid #bababa"}} value={name} onChange={(e) => setName(e.target.value)}/>
+            <div className="cmodal__edit">
+                <div className="tmodal__inputs">
+                    <div className="tmodal__input">
+                        <label htmlFor="tname">{isEn ? "Name" : "Назва"}</label>
+                        <input placeholder="Theme 0" type="text" maxLength="15" className="tmodal__name" id="name" style={{border: error && errorRef.current === 1 ?  "1px solid #c52d2d" : isDark ? "1px solid #353535" : "1px solid #bababa"}} value={name} onChange={(e) => setName(e.target.value)}/>
+                    </div>
+                    <div className="tmodal__input">
+                        <label htmlFor="tname">{isEn ? "Image URL" : "Посилання на зображення"}</label>
+                        <input placeholder="https://images.unsplash.com/photo-1627797427417-ef69f1c4705b?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" type="text" className="tmodal__name" id="url" style={{border: error && errorRef.current === 2 ?  "1px solid #c52d2d" : isDark ? "1px solid #353535" : "1px solid #bababa"}} value={url} onChange={(e) => setUrl(e.target.value)}/>
+                    </div>
                 </div>
-                <div className="tmodal__input">
-                    <label htmlFor="tname">{isEn ? "Image URL" : "Посилання на зображення"}</label>
-                    <input placeholder="https://images.unsplash.com/photo-1627797427417-ef69f1c4705b?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" type="text" className="tmodal__name" id="url" style={{border: error && errorRef.current === 2 ?  "1px solid #c52d2d" : isDark ? "1px solid #353535" : "1px solid #bababa"}} value={url} onChange={(e) => setUrl(e.target.value)}/>
-                </div>
+                {url.length > 0 && <img className="cmodal__img" src={url}/>}
             </div>
             <div className="gmodal__buttons">
                 <motion.div whileHover={{scale: 1.05}} className="tmodal__save" onClick={addTheme}>{isEn ? "Create" : "Створити"}</motion.div>
